d3b6ba1f054644ec324e07215e5ee833ccc88c10
Brandon Donnelson <branflake2267@gmail.com>
2013-01-20 22:14:54 -0800

    jpa mods

--- archetypes/gwt-appengine-guice-jpa1/src/main/java/org/gonevertical/project/client/ProjectEntryPoint.java
+++ archetypes/gwt-appengine-guice-jpa1/src/main/java/org/gonevertical/project/client/ProjectEntryPoint.java
@@ -5,6 +5,7 @@ import org.gonevertical.project.client.rpc.RpcServiceAsync;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -21,6 +22,18 @@ public class ProjectEntryPoint implements EntryPoint {
   @Override
   public void onModuleLoad() {
     RootPanel.get().add(new HTML("loaded"));
+    
+    rpcService.testCallServer("Hello...", new AsyncCallback<String>() {
+      @Override
+      public void onSuccess(String result) {
+        RootPanel.get().add(new HTML(result));
+      }
+      
+      @Override
+      public void onFailure(Throwable caught) {
+        RootPanel.get().add(new HTML("We have a leak Huston!"));
+      }
+    });
   }
   
 }
