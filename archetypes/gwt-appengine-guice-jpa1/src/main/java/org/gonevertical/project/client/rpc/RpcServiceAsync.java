29bcc88c167c541ac98bb0fee5c51cae4a590e06
Brandon Donnelson <branflake2267@gmail.com>
2013-01-20 22:20:03 -0800

    Formatting

--- archetypes/gwt-appengine-guice-jpa1/src/main/java/org/gonevertical/project/client/rpc/RpcServiceAsync.java
+++ archetypes/gwt-appengine-guice-jpa1/src/main/java/org/gonevertical/project/client/rpc/RpcServiceAsync.java
@@ -6,5 +6,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  * The async counterpart of <code>RpcService</code>.
  */
 public interface RpcServiceAsync {
+  
   void testCallServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+  
 }
