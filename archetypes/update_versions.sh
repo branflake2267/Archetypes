b54f9a6ff31a552540efa6abb06cdf393301bd74
Brandon Donnelson <branflake2267@gmail.com>
2013-02-16 21:37:10 -0800

    Pom adjustments

--- archetypes/update_versions.sh
+++ archetypes/update_versions.sh
@@ -7,3 +7,5 @@ find . -name 'pom.xml' -type f -exec sed -i '' 's/<gwt.version>.*<\/gwt.version>
 # Google App engine
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<gae.version>.*<\/gae.version>/<gae.version>1.7.5<\/gae.version>/g' {} \;
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<maven-gae-plugin.version>.*<\/maven-gae-plugin.version>/<maven-gae-plugin.version>0.9.5<\/maven-gae-plugin.version>/g' {} \;
+
+echo "Update Versions Finished"
