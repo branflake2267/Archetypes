136c247d7f0eadab1d04c32516aba0bcab9da7af
Brandon Donnelson <branflake2267@gmail.com>
2013-04-14 00:02:05 -0700

    adding selenium tests

--- archetypes/gwt-basic-cucumber-guice/src/test/java/org/gonevertical/project/cucumber/RunCucumberTestInt.java
+++ archetypes/gwt-basic-cucumber-guice/src/test/java/org/gonevertical/project/cucumber/RunCucumberTestInt.java
@@ -2,7 +2,7 @@ package org.gonevertical.project.cucumber;
 
 import org.junit.runner.RunWith;
 
-import cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 public class RunCucumberTestInt {
---
+++ archetypes/gwt-basic-cucumber-guice/src/test/java/org/gonevertical/project/selenium/Testing.java
@@ -0,0 +1,14 @@
+package org.gonevertical.project.selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
