e6de958d4f89b09e3c5963ee228b1e58b04b6669
branflake2267@gmail.com <branflake2267@gmail.com>
2013-07-22 20:55:23 -0700

    general update

--- archetypes/update_versions.sh
+++ archetypes/update_versions.sh
@@ -3,9 +3,13 @@
 
 # GWT
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<gwt.version>.*<\/gwt.version>/<gwt.version>2.5.1<\/gwt.version>/g' {} \;
+find . -name 'pom.xml' -type f -exec sed -i '' 's/<gwt-maven-plugin.version>.*<\/gwt-maven-plugin.version>/<gwt-maven-plugin.version>2.5.1<\/gwt-maven-plugin.version>/g' {} \;
 
 # Google App engine
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<gae.version>.*<\/gae.version>/<gae.version>1.8.0<\/gae.version>/g' {} \;
-find . -name 'pom.xml' -type f -exec sed -i '' 's/<maven-gae-plugin.version>.*<\/maven-gae-plugin.version>/<maven-gae-plugin.version>0.9.5<\/maven-gae-plugin.version>/g' {} \;
+find . -name 'pom.xml' -type f -exec sed -i '' 's/<maven-gae-plugin.version>.*<\/maven-gae-plugin.version>/<maven-gae-plugin.version>0.9.6<\/maven-gae-plugin.version>/g' {} \;
+
+# GWTP
+find . -name 'pom.xml' -type f -exec sed -i '' 's/<gwtp.version>.*<\/gwtp.version>/<gwtp.version>1.0<\/gwtp.version>/g' {} \;
 
 echo "Update Versions Finished"
--- generated/gwt-basic-rpc-appengine-guice/src/main/resources/archetype-resources/src/main/java/server/guice/ServletPathModule.java
+++ generated/gwt-basic-rpc-appengine-guice/src/main/resources/archetype-resources/src/main/java/server/guice/ServletPathModule.java
@@ -16,7 +16,7 @@ public class ServletPathModule extends ServletModule {
   @Override
   public void configureServlets() {
     // GWT RPC request path
-    serve("/${module}/rpcService*").with(RpcServiceImpl.class);
+    serve("/project/rpcService*").with(RpcServiceImpl.class);
     
     // ignore _ah which is google built in servlets base path
     serveRegex("^/(?!_ah.*)").with(HomeServlet.class);
