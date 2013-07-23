220b5372b72fc41c4625da6fb1ab5f6bae9284a2
branflake2267@gmail.com <branflake2267@gmail.com>
2013-07-22 19:57:55 -0700

    Version update

--- archetypes/update_versions.sh
+++ archetypes/update_versions.sh
@@ -5,7 +5,7 @@
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<gwt.version>.*<\/gwt.version>/<gwt.version>2.5.1<\/gwt.version>/g' {} \;
 
 # Google App engine
-find . -name 'pom.xml' -type f -exec sed -i '' 's/<gae.version>.*<\/gae.version>/<gae.version>1.7.5<\/gae.version>/g' {} \;
+find . -name 'pom.xml' -type f -exec sed -i '' 's/<gae.version>.*<\/gae.version>/<gae.version>1.8.0<\/gae.version>/g' {} \;
 find . -name 'pom.xml' -type f -exec sed -i '' 's/<maven-gae-plugin.version>.*<\/maven-gae-plugin.version>/<maven-gae-plugin.version>0.9.5<\/maven-gae-plugin.version>/g' {} \;
 
 echo "Update Versions Finished"
