8b34cad6817cc6899092c8d4a28ae8ea15337c8a
branflake2267@gmail.com <branflake2267@gmail.com>
2013-08-07 17:43:08 -0700

    Adding remote web test.

---
+++ archetypes/remoteweb-server/src/main/java/com/sencha/gxt/remotewebserver/RunRemoteWebServer.java
@@ -0,0 +1,23 @@
+package com.sencha.gxt.remotewebserver;
+
+import com.google.gwt.junit.remote.BrowserManagerServer;
+
+public class RunRemoteWebServer {
+
+  public static void main(String[] args) {
+    try {
+      new RunRemoteWebServer().run();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void run() throws Exception {
+    String[] args = new String[2];
+    args[0] = "ie";
+    args[1] = "C:\\Program Files\\Internet Explorer\\iexplore.exe";
+    
+    BrowserManagerServer.main(args);
+  }
+
+}
