0f8cf9dda89c8ecba68dcf63994a7491350546ce
branflake2267@gmail.com <branflake2267@gmail.com>
2013-08-07 18:05:11 -0700

    generated selenium-server remoteweb-server and gxt layout with new test archetypes.

--- generated/gxt-gwtp-withlayout/src/main/resources/archetype-resources/src/test/java/client/RunAllGwtTestSuite.java
+++ generated/gxt-gwtp-withlayout/src/main/resources/archetype-resources/src/test/java/client/RunAllGwtTestSuite.java
@@ -11,9 +11,9 @@ import com.google.gwt.junit.tools.GWTTestSuite;
 public class RunAllGwtTestSuite extends GWTTestSuite { 
   public static Test suite() {
     TestSuite suite = new TestSuite("Application Tests");
-    suite.addTestSuite(SimpleUnitTestGwtTest.class);
-    suite.addTestSuite(SeleniumGwtTest.class);
-    suite.addTestSuite(ManualGwtTest.class);
+    suite.addTestSuite(HtmlUnitTestGwtTest.class);
+    suite.addTestSuite(SeleniumUnitRemoteGwtTest.class);
+    suite.addTestSuite(SeleniumUnitIdeGwtTest.class);
     // Add more tests here
     
     return suite;
--- generator/src/main/java/org/gonevertical/archetypes/RunArchetypesGeneratorGxt.java
+++ generator/src/main/java/org/gonevertical/archetypes/RunArchetypesGeneratorGxt.java
@@ -18,7 +18,8 @@ public class RunArchetypesGeneratorGxt {
 //    projects.add("gxt-basic-support-3x");
     projects.add("gxt-gwtp-withlayout");
 //    projects.add("gxt-basic-3x");
-    projects.add("selenium-server");
+    projects.add("selenium-server-runner");
+    projects.add("remoteweb-server-runner");    
 
     
     GwtArchetypeGenerator generate = new GwtArchetypeGenerator();
