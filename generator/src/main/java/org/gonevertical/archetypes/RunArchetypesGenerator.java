769d5a91a8b7ab8541da20861c314144dac9b747
branflake2267@gmail.com <branflake2267@gmail.com>
2014-10-16 19:46:00 -0700

    Adding gwt-basic-snapshot archetype.

--- generator/src/main/java/org/gonevertical/archetypes/RunArchetypesGenerator.java
+++ generator/src/main/java/org/gonevertical/archetypes/RunArchetypesGenerator.java
@@ -13,15 +13,16 @@ public class RunArchetypesGenerator {
 
   private void run() {
     List<String> projects = new ArrayList<String>();
-    projects.add("gwt-basic");
-    projects.add("gwt-basic-rpc");
-    projects.add("gwt-basic-rpc-appengine-guice");
-    projects.add("gwt-basic-requestfactory");
-    projects.add("gwt-activitiesandplaces-requestfactory");
-    projects.add("gwt-activitiesandplaces-requestfactory-maps");
-    projects.add("gwt-css");
-    projects.add("gwt-basic-cucumber-guice");
-    
+    // projects.add("gwt-basic");
+    projects.add("gwt-basic-snapshot");
+    // projects.add("gwt-basic-rpc");
+    // projects.add("gwt-basic-rpc-appengine-guice");
+    // projects.add("gwt-basic-requestfactory");
+    // projects.add("gwt-activitiesandplaces-requestfactory");
+    // projects.add("gwt-activitiesandplaces-requestfactory-maps");
+    // projects.add("gwt-css");
+    // projects.add("gwt-basic-cucumber-guice");
+
     GwtArchetypeGenerator generate = new GwtArchetypeGenerator();
     generate.setProjects(projects);
     generate.setDeploy(true);
